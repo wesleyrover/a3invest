3f9e8a7f2fd2c26c1af6ec4890d430f1bf70f60b
Wesley Rover <wesleyrover@gmail.com>
2023-12-22 23:13:16 -0300

    Criação do componente de footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
     imports: [
       HeaderComponent,
+      FooterComponent,
       CommonModule,
       RouterModule
     ],
-    exports: [HeaderComponent,],
+    exports: [HeaderComponent,FooterComponent,],
     declarations: [],
     providers: []
   })
