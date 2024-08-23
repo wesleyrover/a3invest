e6234b4b88ae95118d86524b8fa6f0b694a2ab10
Wesley Rover <wesleyrover@gmail.com>
2024-08-22 22:54:51 -0300

    rotas

--- src/node/controllers/apiControllers.js
+++ src/node/controllers/apiControllers.js
@@ -1,3 +1,20 @@
 exports.test = function (req, res) {
     res.send("Olá! Teste ao Controller");
-  };
+};
+
+// TODO: listar pontos de interesse da BD
+exports.details = function (req, res) {
+    res.send({ type: "GET" });
+};
+// TODO: adicionar novo ponto de interesse
+exports.add = function (req, res) {
+    res.send({ type: "POST" });
+};
+// TODO: atualizar ponto de interesse
+exports.update = function (req, res) {
+    res.send({ type: "PUT" });
+};
+// TODO: apagar ponto de interesse
+exports.delete = function (req, res) {
+    res.send({ type: "DELETE" });
+};
--- src/node/routes/api.js
+++ src/node/routes/api.js
@@ -5,4 +5,12 @@ const router = express.Router();
 const apiController = require("./../controllers/apiControllers");
 // url do teste será: http://localhost:5000/api/teste
 router.get("/teste", apiController.test);
+// TODO: listar pontos de interesse da BD
+router.get("/details",apiController.details);
+// TODO: adicionar novo ponto de interesse
+router.post("/interest",apiController.add);
+// TODO: atualizar ponto de interesse
+router.put("/interest/:id",apiController.update);
+// TODO: apagar ponto de interesse
+router.delete("/interest/:id",apiController.delete);
 module.exports = router;
